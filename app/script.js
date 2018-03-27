0e30af0e04fe686aded8b4c7dafe4eb1fe9bce19
corinneling <corinne@heysparkbox.com>
2018-03-26 20:57:17 -0400

    refractor to object
    put all object into an object
    and add links querySelectorAll

--- app/script.js
+++ app/script.js
@@ -1,127 +1,96 @@
 // show user control panel
-var toolsButton = document.getElementById('user-tools');
-var toolsSVG = document.getElementById('tools-icon');
+let mainChildren = document.getElementById("main").children;
+let mainDiv = document.getElementById("main");
+let links = document.querySelectorAll('a');
+let p = mainDiv.getElementsByTagName("p");
 
-
-function toggleOptions() {
-    var tools = document.getElementById('accessibility');
-    var displaySetting = tools.style.display;
+const aesthetica11y = {
+  controlPanelEvents: function() {
+    links[0].addEventListener('click', this.toggleOptions);
+    links[1].addEventListener('click', this.togglePageContrast);
+    links[2].addEventListener('click', this.resetPageContrast);
+    links[3].addEventListener('click', this.decreaseFontSize);
+    links[4].addEventListener('click', this.increaseFontSize);
+    links[5].addEventListener('click', this.originalFontSize);
+    links[6].addEventListener('click', this.toggleFontFamily);
+    links[7].addEventListener('click', this.fixLineHeight);
+    links[8].addEventListener('click', this.fixLineSpacing);
+  },
+  toggleOptions: function() {
+    let tools = document.getElementById('accessibility');
+    let displaySetting = tools.style.display;
+    let toolsSVG = document.getElementById('tools-icon');
 
     if (displaySetting == 'block') {
       tools.style.display = 'none';
-			toolsSVG.style.backgroundColor = 'rgb(44, 148, 255)';
+      toolsSVG.style.backgroundColor = 'rgb(44, 148, 255)';
       toolsSVG.style.fill = 'white';
-		  toolsSVG.style.boxShadow = "1px 1px 1px 1px rgba(0,0,0,0.2)"
-    }
-    else {
+      toolsSVG.style.boxShadow = "1px 1px 1px 1px rgba(0,0,0,0.2)"
+    } else {
       tools.style.display = 'block';
-			toolsSVG.style.backgroundColor = 'white';
+      toolsSVG.style.backgroundColor = 'white';
       toolsSVG.style.fill = 'rgb(44, 148, 255)';
-		  toolsSVG.style.boxShadow = "none"
+      toolsSVG.style.boxShadow = "none"
     }
- };
-
-toolsButton.addEventListener('click', toggleOptions);
-
-
-// contrast controls
-let contrastBackground = document.getElementById("dark-contrast");
-let originalBackground = document.getElementById("original-contrast");
-let mainDiv = document.getElementById("main");
-
-
-  function toggleContrast() {
+  },
+  togglePageContrast: function() {
     var contrastBackground = mainDiv.style.backgroundColor;
 
     if (contrastBackground == 'black') {
-			mainDiv.style.backgroundColor = 'white';
-			document.body.style.backgroundColor = 'white';
-		  mainDiv.style.color = 'black';
+      mainDiv.style.backgroundColor = 'white';
+      document.body.style.backgroundColor = 'white';
+      mainDiv.style.color = 'black';
     } else {
       mainDiv.style.backgroundColor = 'black';
-			document.body.style.backgroundColor = 'black';
-		  mainDiv.style.color = 'white';
-		}
-  }
-
-	function resetColors(){
-		mainDiv.style.backgroundColor = "";
-		document.body.style.backgroundColor = "";
-  	mainDiv.style.color = "";
-	}
-contrastBackground.addEventListener('click', toggleContrast);
-originalBackground.addEventListener('click', resetColors);
-
-// font size controls
-let smallFont = document.getElementById("small-font");
-let largeFont = document.getElementById("large-font");
-let originalFont = document.getElementById("refresh-font");
-let mainChildren = document.getElementById("main").children;
-
-function decreaseFontSize(){
-		for (var i = 0; i < mainChildren.length; i++) {
-			childrenFont = window.getComputedStyle(mainChildren[i]).getPropertyValue('font-size');
+      document.body.style.backgroundColor = 'black';
+      mainDiv.style.color = 'white';
+    }
+  },
+  resetPageContrast: function() {
+    mainDiv.style.backgroundColor = "";
+    document.body.style.backgroundColor = "";
+    mainDiv.style.color = "";
+  },
+  decreaseFontSize: function() {
+    for (var i = 0; i < mainChildren.length; i++) {
+      childrenFont = window.getComputedStyle(mainChildren[i]).getPropertyValue('font-size');
       currentSize = parseFloat(childrenFont);
-			console.log(mainChildren[i].style.fontSize = (currentSize - 5) + 'px');
-		}
-};
+      mainChildren[i].style.fontSize = (currentSize - 5) + 'px';
 
-smallFont.addEventListener('click', decreaseFontSize);
-
-function increaseFontSize(){
-		for (var i = 0; i < mainChildren.length; i++) {
-			childrenFont = window.getComputedStyle(mainChildren[i]).getPropertyValue('font-size');
+    }
+  },
+  increaseFontSize: function() {
+    for (var i = 0; i < mainChildren.length; i++) {
+      childrenFont = window.getComputedStyle(mainChildren[i]).getPropertyValue('font-size');
       currentSize = parseFloat(childrenFont);
-			mainChildren[i].style.fontSize = (currentSize + 5) + 'px';
-		}
-};
-
-largeFont.addEventListener('click', increaseFontSize);
-
-function originalFontSize(){
-		for (var i = 0; i < mainChildren.length; i++) {
-			childrenFont = window.getComputedStyle(mainChildren[i]).getPropertyValue('font-size');
+      mainChildren[i].style.fontSize = (currentSize + 5) + 'px';
+    }
+  },
+  originalFontSize: function() {
+    for (var i = 0; i < mainChildren.length; i++) {
+      childrenFont = window.getComputedStyle(mainChildren[i]).getPropertyValue('font-size');
       currentSize = parseFloat(childrenFont);
-			mainChildren[i].style.fontSize = "";
-		}
-};
-
-
-originalFont.addEventListener('click', originalFontSize);
-
-
-let changeFontFamily = document.getElementById("change-family");
-let originalFontFamily = document.getElementById("original-family");
-
-  function toggleFontFamily() {
+      mainChildren[i].style.fontSize = "";
+    }
+  },
+  toggleFontFamily: function() {
     var fontUsed = mainDiv.style.fontFamily;
-
     if (fontUsed == "") {
-			mainDiv.style.fontFamily = "Arial, sans-serif";
+      mainDiv.style.fontFamily = "Arial, sans-serif";
     } else {
-			mainDiv.style.fontFamily = "";
-		}
+      mainDiv.style.fontFamily = "";
+    }
+  },
+  fixLineHeight: function() {
+    for (i = 0; i < p.length; i++) {
+      p[i].style.lineHeight = "200%";
+    }
+  },
+  fixLineSpacing: function() {
+    for (i = 0; i < p.length; i++) {
+      p[i].style.letterSpacing = "1px";
+    }
   }
+}
 
-changeFontFamily.addEventListener('click', toggleFontFamily);
-
-
-let increaseLineHeight = document.getElementById("change-height");
-
-increaseLineHeight.addEventListener('click', function(){
-		let pHeight = mainDiv.getElementsByTagName("p");
-		let i;
-		for (i = 0; i < pHeight.length; i++) {
-    		pHeight[i].style.lineHeight = "200%";
-		}
-});
-
-let increaseSpacing = document.getElementById("increase-spacing");
-
-increaseSpacing.addEventListener('click', function(){
-		let pSpacing = mainDiv.getElementsByTagName("p");
-		let i;
-		for (i = 0; i < pSpacing.length; i++) {
-    		pSpacing[i].style.letterSpacing = "1px";
-		}
-});
+aesthetica11y.controlPanelEvents();
