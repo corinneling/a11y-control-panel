eb29584f23d6a03ac96310a967c3fbd3bea395fb
corinneling <corinne@heysparkbox.com>
2018-03-27 15:36:15 -0400

    add converted bookmarklet code
    to bookmarklet.js

--- script/bookmarklet.js
+++ script/bookmarklet.js
@@ -1,5 +1,8 @@
-javascript: (function () {
-    var jsCode = document.createElement('script');
-    jsCode.setAttribute('src', 'https://corinneling.github.io/aesthetica11y-control-panel/app/script.js');
-    document.body.appendChild(jsCode);
- }());
+javascript:(function()%7Bjavascript%3A%20(function%20()%20%7Bvar%20jsCode%20%3D%20document.createElement('script')%3BjsCode.setAttribute('src'%2C%20'https%3A%2F%2Fcorinneling.github.io%2Faesthetica11y-control-panel%2Fscript%2Faesthetica11yBookmarklet.js')%3Bdocument.body.appendChild(jsCode)%3B%7D())%7D)()
+
+// original
+// javascript: (function () {
+//     var jsCode = document.createElement('script');
+//     jsCode.setAttribute('src', 'https://corinneling.github.io/aesthetica11y-control-panel/script/aesthetica11yBookmarklet.js');
+//     document.body.appendChild(jsCode);
+//  }());
