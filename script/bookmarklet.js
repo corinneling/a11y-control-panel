c79e9b16066e589f07bc2350d7b92aedea951b22
corinneling <corinne@heysparkbox.com>
2018-03-27 13:47:49 -0400

    move html to js
    and restructure project

---
+++ script/bookmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function () {
+    var jsCode = document.createElement('script');
+    jsCode.setAttribute('src', 'https://corinneling.github.io/aesthetica11y-control-panel/app/script.js');
+    document.body.appendChild(jsCode);
+ }());
